8260b36a192469f83fb75fdf0b13789e8486a1cd
AjaiKumar007 <63536390+AjaiKumar007@users.noreply.github.com>
2020-10-21 20:13:49 +0530

    New Vr Project

---
+++ Heaven VR Project/Assets/Script/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void vrgame()
+    {
+        SceneManager.LoadScene("VR");
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void exit()
+    {
+        Application.Quit();
+    }
+
+}
